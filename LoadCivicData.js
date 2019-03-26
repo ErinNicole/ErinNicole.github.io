3aa9e7bce874e86d3e5ba72b257af90e106cbdb9
Erin Shackelford <erinshackelford@Erins-MacBook-Pro.local>
2019-03-26 19:01:16 -0400

    working on scales and axes

--- LoadCivicData.js
+++ LoadCivicData.js
@@ -6,10 +6,11 @@ d3.csv("data/CivicAssocData.csv", function(error, csvData) {
 
 console.log("Large Civic Associations")
 
-var svgWidth = 1200;
-var svgHeight = 400;
-var scaleFactor = 3;
+var svgWidth = 800;
+var svgHeight = 500;
+//var scaleFactor = .1;
 
+//Draw the Bar Chart
 function barChart(barData) {
 
   var svg = d3.selectAll('.barChart')
@@ -26,54 +27,37 @@ function barChart(barData) {
   
   var xScale = d3.scaleLinear()
     .domain([minYear, maxYear])
-    .range([0, window.svgWidth]);  
+    .range([0, svgWidth - 25]);
 
   var bars = svg.selectAll('rect')
     .data(barData);
 
   bars.enter().append("rect")
-    .attr("height", 10)
+    .attr("height", 5)
     .attr("width", function(d) {
       console.log(d)
-        return d["YEARS IN EXISTENCE"] * scaleFactor;
+    return d["YEARS IN EXISTENCE"];
+//        return d["YEARS IN EXISTENCE"] * scaleFactor;
+    })
+    .text(function(d) {return (parseFloat(d["ASSOCIATION"]))
     })
     .attr('y', function(d, i) {
-      return i * 20;
+      return i * 10;
     })
     .attr('x', function(d, i) {
       return xScale(parseFloat(d["FOUNDING YEAR"]));
     })
 
-  var dataExtent = d3.extent(barData, function(d) {
-      return d["FOUNDING YEAR"];
-      });
-  }
+    var axis = d3.axisBottom(xScale);
+     d3.select("#xAxis").call(axis);
+
+    var xCoordinate = svgWidth * i;
+      return "translate("+ xCoordinate +")";
+}
 
 
-
-//var dataExtent = d3.extent(data, function(d) {
-//    return d["FOUNDING YEAR"];
-
-//var barWidth = (svgWidth / data.length);
-//var a = parseFloat("FOUNDING_YEAR");
-//var b = parseFloat("ENDING_YEAR");
-
-//var barChart = svg.select("barChart")
-//var rect = svg.selectAll("rect")
-//  .data(data)
-//var barWidth = 10
-//var barPadding = 5
-
-//function barChart() {
-//  rect.enter().append("rect")  
-//    .attr("y", function(d)["FOUNDING_YEAR"]) 
-//      return svgHeight - d
-//    .attr("height", function(d) {
-//      return d;
-//    })
-//    .attr("width", function(d)["ENDING_YEAR"])
-//    .attr("transform", function (d, i) {
-//    });
+//  var dataExtent = d3.extent(barData, function(d) {
+//      return d["FOUNDING YEAR"];
 
 //d3.selectAll("rect")
 //  .on("click", function(d) {
@@ -91,31 +75,4 @@ function barChart(barData) {
 //    d3.select("#tooltip")
 //        .style("display", "none")
 //  });
-
-//var axis = d3.axisBottom(xScale);
-//d3.select("#xAxis").call(axis);
-
-//var xCoordinate = barWidth * i;
-//  return "translate("+ xCoordinate +")";
-
-
-
-//d3.select("containerOne")
-//    .selectAll("rectOne").style("color", function(d, i) {return i % 2 ? "rgb(227,38,42)" : "rgb(41,171,226)";})
-//    .data()
-//    .enter().append("rectOne")
-//      .text(function(d) { return "ASSOCIATION" + d + "!"; });
-
-// Update...
-//  var rectOne = d3.select("containerOne")
-//    .selectAll("rectOne")
-//    .data()
-//      .text(function(d) { return d; });
-
-// Enter...
-//  rectOne.enter().append("containerOne")
-//    .text(function(d) { return d; });
-
-// Exit...
-//  rectOne.exit().remove();
 
