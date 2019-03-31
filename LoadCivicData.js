3543544fae76c8f1dcbae21e225b228ddddfe1d3
Erin Shackelford <erinshackelford@Erins-MacBook-Pro.local>
2019-03-31 11:00:22 -0400

    styling tooltip

--- LoadCivicData.js
+++ LoadCivicData.js
@@ -27,7 +27,7 @@ function barChart(barData) {
   
   var xScale = d3.scaleLinear()
     .domain([minYear, maxYear])
-    .range([0, svgWidth - 25]);
+    .range([0, svgWidth]);
 
   var bars = svg.selectAll('rect')
     .data(barData);
@@ -36,43 +36,47 @@ function barChart(barData) {
     .attr("height", 5)
     .attr("width", function(d) {
       console.log(d)
-    return d["YEARS IN EXISTENCE"];
-//        return d["YEARS IN EXISTENCE"] * scaleFactor;
+      return xScale(minYear + parseFloat(d["YEARS IN EXISTENCE"]));
     })
     .attr('y', function(d, i) {
       return i * 10;
     })
     .attr('x', function(d, i) {
-      return xScale(parseFloat(d["FOUNDING YEAR"]));
+      return xScale(parseFloat(d["FOUNDING YEAR"]))
     })
+    .on("mousemove", function(d) {
+      var mouse = d3.mouse(document.body);
+      d3.select("#tooltip")
+          .style("display", "block")
+          .html("<p>" + d["ASSOCIATION"] + "</p>")
+          .style("left", mouse[0] + 20 + "px")
+          .style("top", mouse[1] - 50 + "px");
+    })
+    .on("mouseout", function(d) {
+      d3.select("#tooltip")
+          .style("display", "none")
+    })
+
+//  var colorScale = d3.scale.ordinal()
+//    .domain(d["ASSOCIATION TYPE"])
+//    .range(['#ddd', 'red'])
+
     .attr("fill", "steelblue");
 
-    var axis = d3.axisBottom(xScale);
-     d3.select("#xAxis").call(axis);
+  var labels = svg.selectAll('text')
+    .data(barData);
 
-    var xCoordinate = svgWidth * i;
-      return "translate("+ xCoordinate +")";
+  labels.enter().append("text")
+    .text(function(d) {
+      return d["ASSOCIATION"];
+    })
+    .attr('y', function(d, i) {
+      return i * 10;
+    })
+    .attr('x', 0);
 
+  var xAxis = d3.axisBottom(xScale)
+    // .tickFormat;
+     d3.select("#xAxis").call(xAxis);
 
-
-
-//  var dataExtent = d3.extent(barData, function(d) {
-//      return d["FOUNDING YEAR"];
-
-//d3.selectAll("rect")
-//  .on("click", function(d) {
-//    console.log(d);
-//  })
-//  .on("mousemove", function(d) {
-//    var mouse = d3.mouse(this);
-//    d3.select("#tooltip")
-//        .style("display", "block")
-//        .html("<h1>" + d + "</h1>")
-//        .style("left", mouse[0] + "px")
-//        .style("top", mouse[1] - 50 + "px");
-//  })
-//  .on("mouseout", function(d) {
-//    d3.select("#tooltip")
-//        .style("display", "none")
-//  });
 }
