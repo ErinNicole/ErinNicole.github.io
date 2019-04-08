769870df60618665b5faf58ab6348bbe872f50bf
Erin Shackelford <erinshackelford@Erins-MacBook-Pro.local>
2019-04-07 21:44:47 -0400

    Trying to nest data and make a key for bar chart

--- LoadCivicData.js
+++ LoadCivicData.js
@@ -2,14 +2,13 @@
 d3.csv("data/CivicAssocData.csv", function(error, csvData) {
   console.log("csv:", csvData);
   barChart(csvData);
+  barChartKey(csvData);
 });
 
 console.log("Large Civic Associations")
 
 var svgWidth = 1400;
 var svgHeight = 600;
-
-//var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 var color = d3.scaleOrdinal()
     .range(["rgb(89, 190, 201)", "rgb(0, 132, 213)", "rgb(214, 0, 28)", "rgb(41, 127, 202)", "rgb(124, 37, 41)", "rgb(255, 205, 0)", "rgb(255, 143, 28)", "rgb(0, 170, 19)", "rgb(40, 97, 64)", "rgb(51, 0, 114)", "rgb(23, 74, 91)", "rgb(170, 219, 30)"]);
@@ -84,3 +83,44 @@ function barChart(barData) {
     d3.select("#xAxis").call(xAxis);
 }
 
+//Creating Bar Chart Key
+
+//d3.csv("data/CivicAssocData.csv", function(error, csvData) {
+  // var categories = d3.nest()
+  //     .key(function(d) { return d["ASSOCIATION TYPE"]; 
+  //   })
+  //     .entries(csvData);
+
+function barChartKey(barData) {
+  var svg = d3.selectAll('.barChartKey')
+  .attr("width", 400)
+  .attr("height", 100)
+  .style("display", "inline-block");
+
+
+  var categories = svg.select("#categories").selectAll("text")
+  .data(barData);
+  categories.enter().append("text")
+    .text(function(d) {
+      return d["ASSOCIATION TYPE"];
+    })
+    .attr('y', function(d, i) {
+      return i * 12;
+    })
+//    .attr('dy', 7)
+//    .attr('x', 670);
+
+  var swatch = svg.selectAll('rect')
+  .data(barData);
+
+  swatch.enter().append("rect")
+  .attr("height", 8)
+  .attr("width", 8)
+  .attr('y', function(d, i) {
+    return i * 12;
+  })
+  .attr('x', 5)
+  .attr("fill", function(d) {
+      return color(d["ASSOCIATION TYPE"]);
+  })
+}
