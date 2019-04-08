bde12db05df2b72a3a9829bd66bdaed2ae9f8f5d
Erin Shackelford <erinshackelford@Erins-MacBook-Pro.local>
2019-04-08 15:08:14 -0400

    colors

--- LoadCivicData.js
+++ LoadCivicData.js
@@ -2,16 +2,20 @@
 d3.csv("data/CivicAssocData.csv", function(error, csvData) {
   console.log("csv:", csvData);
   barChart(csvData);
-  barChartKey(csvData);
+  var categories = d3.nest()
+    .key(function(d) {
+      return d["ASSOCIATION TYPE"]; 
+  })
+    .entries(csvData);
+  barChartKey(categories);
 });
 
 console.log("Large Civic Associations")
 
 var svgWidth = 1400;
 var svgHeight = 600;
-
 var color = d3.scaleOrdinal()
-     .range(["#3399ff", "#66ccff", "#99cccc", "#cccccc", "#6666ff", "#9999ff", "#cc99ff", "#ccccff", "#993399", "#ff6699", "#ff3333", "#cc3333"]);
+     .range(["#3399ff", "#66ccff", "#99ffff", "#66cccc", "#6666ff", "#9999ff", "#cc99ff", "#ccccff", "#993399", "#ff6699", "#ff3333", "#cc3333"]);
 
 
 //Draw the Bar Chart
@@ -83,21 +87,34 @@ function barChart(barData) {
     .tickFormat(d3.format("d"));
     d3.select("#xAxis").call(xAxis);
 }
+// gridlines in x axis function
+function make_x_gridlines() {		
+  return d3.axisBottom(x)
+      .ticks(5)
+}
+  var grid = svg.selectAll('tick')
+  .data(barData);
 
- //Creating Bar Chart Key
+  grid.enter().append("tick")
+  .attr("height", svgHeight)
+  .attr("width", 1)
+  .attr('y', axisBottom)
+  .attr('x', function(d, i) {
+    return make_x_gridlines()
+  })
 
-// d3.csv("data/CivicAssocData.csv", function(error, csvData) {
-//   var categories = d3.nest()
-//       .key(function(d) { return d["ASSOCIATION TYPE"]; 
-//     })
-//       .entries(csvData);
+svg.append("g")			
+    .attr("class", "grid")
+    .call(make_x_gridlines()
+    .tickSize(-height)
+    .tickFormat("")
+)
 
 function barChartKey(barData) {
   var svg = d3.selectAll('.barChartKey')
   .attr("width", 400)
   .attr("height", 100)
   .style("display", "inline-block");
-
 
   var categories = svg.select("#categories").selectAll("text")
   .data(barData);
