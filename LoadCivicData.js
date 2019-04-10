bc49b118d5e0d4504223bdb2bce1bde864985440
Erin Shackelford <erinshackelford@Erins-MacBook-Pro.local>
2019-04-09 22:46:36 -0400

    attempted a second graph then commented it out

--- LoadCivicData.js
+++ LoadCivicData.js
@@ -2,13 +2,20 @@
 d3.csv("data/CivicAssocData.csv", function(error, csvData) {
   console.log("csv:", csvData);
   barChart(csvData);
-  var categories = d3.nest()
+  var categoriesA = d3.nest()
     .key(function(d) {
       return d["ASSOCIATION TYPE"]; 
-  })
+    })  
     .entries(csvData);
-  barChartKey(categories);
-});
+    barChartKey(categoriesA);
+  });
+
+  // var categoriesB = d3.nest()
+  //   .key(function(d) {
+  //     return d["SCOPE AT INCEPTION"];
+  //   })
+  //   .entries(csvData);
+  //   barChartKeyB(categoriesB);
 
 console.log("Large Civic Associations")
 
@@ -16,9 +23,11 @@ var svgWidth = 1400;
 var svgHeight = 600;
 var color = d3.scaleOrdinal()
      .range(["#3399ff", "#66ccff", "#99ffff", "#66cccc", "#6666ff", "#9999ff", "#cc99ff", "#ccccff", "#993399", "#ff6699", "#ff3333", "#cc3333"]);
+//var colorB = d3.scaleOrdinal()
+//     .range(["#3399ff", "#66ccff", "#99ffff", "#66cccc", "#6666ff", "#9999ff", "#cc99ff", "#ccccff", "#993399", "#ff6699", "#ff3333", "#cc3333"]);
 
 
-//Draw the Bar Chart
+//Draw Bar Chart A
 function barChart(barData) {
 
   var svg = d3.selectAll('.barChart')
@@ -68,7 +77,8 @@ function barChart(barData) {
     .attr("fill", function(d) {
         return color(d["ASSOCIATION TYPE"]);
     })
-    
+
+    //Creating a tooltip
     .on("mousemove", function(d) {
       var mouse = d3.mouse(document.body);
       d3.select("#tooltip")
@@ -83,11 +93,12 @@ function barChart(barData) {
           .style("display", "none")
     })
 
+  //Creating the X Axis  
   var xAxis = d3.axisBottom(xScale)
     .tickFormat(d3.format("d"));
     d3.select("#xAxis").call(xAxis);
 }
-// gridlines in x axis function
+  //Drawing gridlines in X Axis function
 function make_x_gridlines() {		
   return d3.axisBottom(x)
       .ticks(5)
@@ -109,7 +120,7 @@ svg.append("g")
     .tickSize(-height)
     .tickFormat("")
 )
-
+//Creating a Key for Bar Chart A
 function barChartKey(barData) {
   var svg = d3.selectAll('.barChartKey')
   .attr("width", 400)
@@ -141,4 +152,74 @@ function barChartKey(barData) {
   .attr("fill", function(d) {
       return color(d["ASSOCIATION TYPE"]);
   })
+
+//Draw Bar Chart B
+// function barChartB(barData) {
+
+//   var svg = d3.selectAll('.barChartB')
+//     .attr("width", svgWidth)
+//     .attr("height", svgHeight);
+
+//  var minYear = d3.min(barData, function(d) {
+//      return parseFloat(d["FOUNDING YEAR"])
+//    });
+
+//  var maxYear = d3.max(barData, function(d) {
+//      return parseFloat(d["ENDING YEAR"])
+//    });
+  
+  // var xScale = d3.scaleLinear()
+  //   .domain([minYear, maxYear])
+  //   .range([10, 650]);
+
+  // var labels = svg.select("#labels").selectAll("text")
+  //   .data(barData);
+
+  // labels.enter().append("text")
+  //   .text(function(d) {
+  //     return d["ASSOCIATION"];
+  //   })
+  //   .attr('y', function(d, i) {
+  //     return i * 12;
+  //   })
+  //   .attr('dy', 7)
+  //   .attr('x', 670);
+
+//  var bars = svg.selectAll('rect')
+//    .data(barData);
+
+  // bars.enter().append("rect")
+  //   .attr("height", 8)
+  //   .attr("width", function(d) {
+  //     console.log(d)
+  //     return xScale(minYear + parseFloat(d["YEARS IN EXISTENCE"]));
+  //   })
+  //   .attr('y', function(d, i) {
+  //     return i * 12;
+  //   })
+  //   .attr('x', function(d, i) {
+  //     return xScale(parseFloat(d["FOUNDING YEAR"]))
+  //   })
+  //   .attr("fill", function(d) {
+  //       return colorB(d["SCOPE AT INCEPTION"]);
+  //   })
+    
+  //   .on("mousemove", function(d) {
+  //     var mouse = d3.mouse(document.body);
+  //     d3.select("#tooltip")
+  //         .style("display", "inline-block")
+  //         .style("postion", "relative")
+  //         .html("<div class='tooltip-title'>" + d["ASSOCIATION"] + "<br>" + " Scope at Inception:  " + d["SCOPE AT INCEPTION"] + "<br>" + " Founding Year:  " + d["FOUNDING YEAR"] + "<br>" + " Ending Year:  " + d["ENDING YEAR"] + "</br>" + "</div>")
+  //         .style("left", mouse[0] + 20 + "px")
+  //         .style("top", mouse[1] - 50 + "px");
+  //   })
+  //   .on("mouseout", function(d) {
+  //     d3.select("#tooltip")
+  //         .style("display", "none")
+  //   })
+
+  // var xAxis = d3.axisBottom(xScale)
+  //   .tickFormat(d3.format("d"));
+  //   d3.select("#xAxis").call(xAxis);
 }
+
