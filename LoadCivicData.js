aed4a98941ab51260811c4c1a2cf14085bad7449
Erin Shackelford <erinshackelford@Erins-MacBook-Pro.local>
2019-04-23 12:07:12 -0400

    adding + to tooltip ending years

--- LoadCivicData.js
+++ LoadCivicData.js
@@ -71,7 +71,7 @@ function barChart(barData) {
   d3.select("#tooltip")
     .style("display", "inline-block")
     .style("postion", "relative")
-    .html("<div class='tooltip-title'>" + d["ASSOCIATION"] + "<br>" + " Association Type:  " + d["ASSOCIATION TYPE"] + "<br>" + " Founding Year:  " + d["FOUNDING YEAR"] + "<br>" + " Ending Year:  " + d["ENDING YEAR"] + "</br>" + "</div>")
+    .html("<div class='tooltip-title'>" + d["ASSOCIATION"] + "<br>" + " Association Type:  " + d["ASSOCIATION TYPE"] + "<br>" + " Founding Year:  " + d["FOUNDING YEAR"] + "<br>" + " Ending Year:  " + d["ENDING YEAR"] + " + " + "</br>" + "</div>")
     .style("left", mouse[0] + 20 + "px")
     .style("top", mouse[1] - 50 + "px");
 })
@@ -129,81 +129,8 @@ function barChartKey(barData) {
       return i * 12;
     })
     .attr('x', 150)
-    
-    // function(d, i) {
-    //   return i * 12;
-    // })
     .attr("fill", function(d) {
         return color(d.key);
     });
 }
-//Draw Bar Chart B
-// function barChartB(barData) {
-
-//   var svg = d3.selectAll('.barChartB')
-//     .attr("width", svgWidth)
-//     .attr("height", svgHeight);
-
-//  var minYear = d3.min(barData, function(d) {
-//      return parseFloat(d["FOUNDING YEAR"])
-//    });
-
-//  var maxYear = d3.max(barData, function(d) {
-//      return parseFloat(d["ENDING YEAR"])
-//    });
-  
-  // var xScale = d3.scaleLinear()
-  //   .domain([minYear, maxYear])
-  //   .range([10, 650]);
-
-  // var labels = svg.select("#labels").selectAll("text")
-  //   .data(barData);
-
-  // labels.enter().append("text")
-  //   .text(function(d) {
-  //     return d["ASSOCIATION"];
-  //   })
-  //   .attr('y', function(d, i) {
-  //     return i * 12;
-  //   })
-  //   .attr('dy', 7)
-  //   .attr('x', 670);
-
-//  var bars = svg.selectAll('rect')
-//    .data(barData);
-
-  // bars.enter().append("rect")
-  //   .attr("height", 8)
-  //   .attr("width", function(d) {
-  //     console.log(d)
-  //     return xScale(minYear + parseFloat(d["YEARS IN EXISTENCE"]));
-  //   })
-  //   .attr('y', function(d, i) {
-  //     return i * 12;
-  //   })
-  //   .attr('x', function(d, i) {
-  //     return xScale(parseFloat(d["FOUNDING YEAR"]))
-  //   })
-  //   .attr("fill", function(d) {
-  //       return colorB(d["SCOPE AT INCEPTION"]);
-  //   })
-    
-  //   .on("mousemove", function(d) {
-  //     var mouse = d3.mouse(document.body);
-  //     d3.select("#tooltip")
-  //         .style("display", "inline-block")
-  //         .style("postion", "relative")
-  //         .html("<div class='tooltip-title'>" + d["ASSOCIATION"] + "<br>" + " Scope at Inception:  " + d["SCOPE AT INCEPTION"] + "<br>" + " Founding Year:  " + d["FOUNDING YEAR"] + "<br>" + " Ending Year:  " + d["ENDING YEAR"] + "</br>" + "</div>")
-  //         .style("left", mouse[0] + 20 + "px")
-  //         .style("top", mouse[1] - 50 + "px");
-  //   })
-  //   .on("mouseout", function(d) {
-  //     d3.select("#tooltip")
-  //         .style("display", "none")
-  //   })
-
-  // var xAxis = d3.axisBottom(xScale)
-  //   .tickFormat(d3.format("d"));
-  //   d3.select("#xAxis").call(xAxis);
-// }
 
